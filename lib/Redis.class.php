f0bffd9cc706e323bfda38b1b063a7567a6c94e6
cy <cy@cydeMacBook-Pro.local>
2016-08-19 23:49:11 +0800

    redis

--- lib/Redis.class.php
+++ lib/Redis.class.php
@@ -1,7 +1,18 @@
 <?php
 class Lib_Redis {
+	private $redis;
 	public function construct(){
-		echo "init ";
+		$this->redis = new Redis();
+		$this->redis = new redis();  
+		$this->redis->connect('127.0.0.1', 6379);  
 	}
+
+	public function setValue(){
+		$result = $this->redis->set('test',"11111111111");  
+		var_dump($result);    //结果：bool(true)  
+	}
+
+
+
 }
 
